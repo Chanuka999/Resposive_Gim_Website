aec306a0921c1178bbf78521f68f63907d018923
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-01 18:33:31 +0530

    add offer.jsx page in app.jsx

--- gim_website/src/App.js
+++ gim_website/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Feature from "./Components/Feature";
+import Offer from "./Components/Offer";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Feature />
+      <Offer />
     </div>
   );
 };
