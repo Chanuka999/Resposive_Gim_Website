f35033cc8cb7c9b48bccd9b2e547261d3b0c4e5f
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-01 10:21:24 +0530

    add

--- gim_website/src/App.js
+++ gim_website/src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React from "react";
+import Navbar from "./Components/Navbar";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
 
 export default App;
