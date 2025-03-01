b67465634695e15f2dbb22d4704071af905077da
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-01 18:33:42 +0530

    update page

--- gim_website/src/Components/Feature.jsx
+++ gim_website/src/Components/Feature.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Featurebox from "./Featurebox";
+import fimage1 from "../images/1.svg";
+import fimage2 from "../images/2.svg";
+import fimage3 from "../images/3.svg";
+import fimage4 from "../images/4.svg";
 
 function Feature() {
-  return <div></div>;
+  return (
+    <div id="features">
+      <h1>FEATURES</h1>
+      <div className="a-container">
+        <Featurebox image={fimage1} title="weightLifting" />
+        <Featurebox image={fimage2} title="specific Music" />
+        <Featurebox image={fimage3} title="Flex your music" />
+        <Featurebox image={fimage4} title="Cardie Exercise" />
+      </div>
+    </div>
+  );
 }
 
 export default Feature;
