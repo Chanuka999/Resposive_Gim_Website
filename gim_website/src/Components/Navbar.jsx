92fb2753ca576ad033a51f753c651e83a787bfff
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-01 10:22:12 +0530

    create navbar page

---
+++ gim_website/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import logo from "../images/logo.png";
+import { Link } from "react-scroll";
+
+function Navbar() {
+  return <div></div>;
+}
+
+export default Navbar;
