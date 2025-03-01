ab6d4a573c9e410cadc09a4dd9cbd70d4a59bb9a
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-01 19:50:00 +0530

    update

--- gim_website/src/Components/Offer.jsx
+++ gim_website/src/Components/Offer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Offer() {
-  return <div></div>;
+  return (
+    <div id="offer">
+      <div className="pr-heading">
+        <h1>
+          A BIG<span>OFFER</span>FOR THIS SUMMER
+        </h1>
+        <p className="details">
+          Free Personal Training Session – Sign up now and get a free one-on-one
+          session with a certified trainer.{" "}
+        </p>
+        <div className="pr-btns">
+          <a href="#" className="pr-btn">
+            JOIN NOW
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Offer;
